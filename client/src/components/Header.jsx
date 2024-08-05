f789c92f3b0d08b9ace4219ceb385aaf63f0829c
chalbrik <64196949+chalbrik@users.noreply.github.com>
2024-08-05 14:23:23 +0200

    Header added

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>MyWorkout</h1>
+      <nav>
+        <ul>
+          <li>Log out</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,19 @@
 import express from "express";
 
 const app = express();
-const port = 3010;
+const port = 5000;
+
+app.get("/api", (req, res) => {
+  res.json({ users: ["userOne", "userTwo", "userThree"] });
+});
+
+// na pewno będzie GET do strony internetowej
+// GET do zalogownia
+// GET do wylogowania
+// POST do umieszczenia karty treningowej
+// DELETE do usunięcia karty treningowej
+// POST do umieszczenia ćwiczenia
+// DELETE do usunięcia ćwiczenia
 
 app.listen(port, (req, res) => {
   console.log(`The app is running on port ${port}`);
