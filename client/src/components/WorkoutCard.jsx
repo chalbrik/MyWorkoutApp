3f756cd2385cde1f9f32fde8d44cf868e12c8415
chalbrik <64196949+chalbrik@users.noreply.github.com>
2024-08-06 15:00:33 +0200

    Adding title fixed

--- client/src/components/WorkoutCard.jsx
+++ client/src/components/WorkoutCard.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function WorkoutCard(props) {
+  const [workoutCardData, setWorkoutCardData] = useState({
+    workoutTitle: "",
+    exercise: "",
+    series: "",
+    repetitions: "",
+    weight: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    console.log(event.target);
+
+    setWorkoutCardData((prevData) => {
+      return {
+        ...prevData,
+        [name]: value,
+      };
+    });
+
+    event.preventDefault();
+  }
+
   return (
     <div className="workout-card">
       <input
-        type="text"
+        onChange={handleChange}
         name="workoutTitle"
         placeholder={props.workoutTitle}
-        value=""
+        value={workoutCardData.workoutTitle}
       />
       <ul>
         <li>Exercise</li>
